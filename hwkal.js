eb10d0d4cab2886df45f808f4e1461fdd72be55e
gesha20129 <91309083+gesha20129@users.noreply.github.com>
2022-10-22 00:08:44 +0700

    Update hwkal.js

--- hwkal.js
+++ hwkal.js
@@ -13,25 +13,25 @@ global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutp
 //=================================================//
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
-global.youtube = 'https://youtube.com/c/RisalTmy'
-global.ig = 'https://www.instagram.com/hikal_857/?hl=id'
-global.mygc = 'wa.me/62895330006855'
+global.youtube = 'https://youtube.com/c/kizaru'
+global.ig = 'https://www.instagram.com/geshrfd/?hl=id'
+global.mygc = 'wa.me/6285891318105'
 global.myweb = 'https://eclass.iainsalatiga.ac.id/app/upload/users/1/10892/my_files/XhiroMhonshine.html'
 global.linkgrupss = "https://chat.whatsapp.com/Jh7zaCSnPCBC4gdYOEVm0K"
 global.linkpro = 'mailto:support@whatsapp.com?subject=Deactivate%20My%20Account%20Number&body=Please%20Deactivate%20The%20My%20Account%20Number%20%2B62XXXXXXXX%20Immediately%20Because%20The%20Number%20Has%20Been%20Lost'
 global.email = 'scytzall01@gmail.com'
 global.region = 'indonesia'
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'ZALL MODS WA' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'ZALL MODS WA' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'ZALL MODS WA' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'Anu BOTZ' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'Anu BOTZ' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'Anu BOTZ' //ubah jadi nama mu, note tanda ' gausah di hapus!
 //=================================================//
 // Other
-global.owner = ['6281214281312']
-global.premium = ['6281214281312']
-global.packname = 'ZALL MODS WA'
-global.ownerr = ['ZALL MODS WA']
-global.author = 'ZALL MODS WA'
+global.owner = ['6285891318105']
+global.premium = ['6285891318105']
+global.packname = 'Anu BOTZ'
+global.ownerr = ['Anu BOTZ']
+global.author = 'Anu BOTZ'
 global.sessionName = './sessionye/zall'
 global.prefa = ['','!','.',',','🐤','🗿']
 global.sp = '⭔'
